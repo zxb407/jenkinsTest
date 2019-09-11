b0f5c7ab440cfe825654d7d03a0161a397074dd9
VIP\xbzhang <xbzhang@i9i8.com>
2019-09-11 19:39:50 +0800

    seventth committed

---
+++ app/src/main/java/com/example/jenkinstestdemo/ui/SecondActivity.kt
@@ -0,0 +1,21 @@
+package com.example.jenkinstestdemo.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import com.example.jenkinstestdemo.R
+
+/**
+ * PackageName: com.example.jenkinstestdemo.ui
+ * Created by xbzhang on 2019/9/11
+ * Description:
+ *
+ */
+class SecondActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_layout)
+    }
+
+}
